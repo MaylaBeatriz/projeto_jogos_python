5d1e6ffe0865f51c8776356496446db82493e72b
Danilo DOrgam <danilodorgam@gmail.com>
2023-11-24 14:13:41 -0300

    Guarda as letras acertadas

--- forca.py
+++ forca.py
@@ -5,20 +5,23 @@ def jogar():
             '***************************')
 
       palavra_secreta = 'banana'
+      letras_acertadas = ['_','_','_','_','_','_']
       enforcou = False
       acertou = False
 
       while(not enforcou and not acertou):
 
             chute = input('Qual letra? ')
+            chute = chute.strip()
 
             index = 0
 
             for letra in palavra_secreta:
-                  if(chute == letra):
-                        print(f'Encontrei a letra {letra} na posição {index}')
+                  if(chute.upper() == letra.upper()):
+                        letras_acertadas[index] = letra
                   index = index + 1
 
+            print(letras_acertadas)
       print('Fim do jogo')
 
 if(__name__ == '__main__'):
