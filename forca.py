fb5c928ac3e5f69b68056aabd1311a63d2025b50
Danilo DOrgam <danilodorgam@gmail.com>
2023-11-27 15:48:00 -0300

    Definição do número de tentativas

--- forca.py
+++ forca.py
@@ -6,22 +6,32 @@ def jogar():
 
       palavra_secreta = 'banana'
       letras_acertadas = ['_','_','_','_','_','_']
+      letras_faltando = str(letras_acertadas.count('_'))
       enforcou = False
       acertou = False
+      tentativas = 0
+
+      print(letras_acertadas)
 
       while(not enforcou and not acertou):
 
             chute = input('Qual letra? ')
             chute = chute.strip()
 
-            index = 0
+            if(chute in palavra_secreta):
+                  index = 0
+                  for letra in palavra_secreta:
+                        if(chute.upper() == letra.upper()):
+                              letras_acertadas[index] = letra
+                        index += 1
+            else:
+                  tentativas += 1
 
-            for letra in palavra_secreta:
-                  if(chute.upper() == letra.upper()):
-                        letras_acertadas[index] = letra
-                  index = index + 1
+            enforcou = tentativas == 6
 
             print(letras_acertadas)
+            print(f'Ainda falta acertar {letras_faltando} letras')
+
       print('Fim do jogo')
 
 if(__name__ == '__main__'):
